b436015bbf2cfe3d8a676a6374319b4469bc4c38
btea <2356281422@qq.com>
2018-08-27 19:15:05 +0800

    .

--- src/components/user.vue
+++ src/components/user.vue
@@ -4,6 +4,10 @@
         <div>我是User组件</div>
         <div>动态部分{{dynamicSegment}}</div>
         <textarea ref="text" style="margin:0;padding:0;overflow:hidden;word-warp:break-word;resize:none;width:300px;border:1px solid #6cf;border-radius:5px" @input="getChange" :style="{height: height + 'px'}"></textarea>
+        <br>
+        <div @click="addClass" :class="{close: active}" class="trans"></div>
+        <br>
+        <div style="height:300px;width:150px;position:relative;"><div class="ani" :class="{aniClose: close}" @click="addClose"></div></div>
     </div>
 </template>
 <script>
@@ -22,12 +26,20 @@ export default {
                 this.height = scrollHeight
             }
 
+        },
+        addClass: function(){
+            this.active = true;
+        },
+        addClose: function(){
+            this.close = true;
         }
     },
 
     data: function(){
         return {
-            height: 50
+            height: 50,
+            active: false,
+            close: false
         }
     },
 
@@ -53,3 +65,26 @@ export default {
     }
 }
 </script>
+<style scoped>
+    .trans{
+        height: 100px;
+        width: 50px;
+        background-color: #6cf;
+    }
+    .close{
+        height: 0;
+        transition: all 1.5s;
+    }
+    .ani{
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        width: 100%;
+        background: #6cf;
+    }
+    .aniClose{
+        top: 100%;
+        transition: all 1.5s;
+    }
+</style>
+
